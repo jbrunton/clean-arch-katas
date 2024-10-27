c99ceefae29903cf9effae4dd38ff9cdafd8458a
John Brunton <1276413+jbrunton@users.noreply.github.com>
2024-10-27 12:00:23 +0000

    chore: remove setup dir

--- setup/nodejs/src/domain/entities/subject.ts
+++
@@ -1,3 +0,0 @@
-export type Subject = {
-  name: string;
-};
--- setup/nodejs/src/domain/usecases/greet.ts
+++
@@ -1,5 +0,0 @@
-import { Subject } from "entities/subject";
-
-export const greet = (subject: Subject): void => {
-  console.info(`Hello, ${subject.name}`);
-};
